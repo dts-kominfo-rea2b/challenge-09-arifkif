97122d3d7b9344d8d8b2247bdb2c7447b0f15009
arifkif <rifki738@gmail.com>
2022-06-26 02:27:17 +0700

    tugas 11 fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,16 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = ({ data }) => {
+  return (
+    <div className="card">
+      <img className="photo" src={data.photo} alt="foto"></img>
+      <h3>{data.name}</h3>
+      <p>{data.phone}</p>
+      <p>{data.email}</p>
+    </div>
+  );
+};
 
 export default Contact;
